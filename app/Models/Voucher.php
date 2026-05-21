2bdba6c3a7f278e3afa9c248b66fdef7e8d47c20
thehanifz <thehanifz@gmail.com>
2026-05-21 13:19:21 +0700

    fix: remove SoftDeletes from Voucher - deleted_at column not in table

--- app/Models/Voucher.php
+++ app/Models/Voucher.php
@@ -4,12 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Voucher extends Model
 {
-    use SoftDeletes;
-
     protected $table = 'vouchers';
 
     protected $fillable = [
